36d6bbc9e522e67d828b8b0dfc0d49ee022009e2
bsbarbara17 <89658143+bsbarbara17@users.noreply.github.com>
2022-06-27 22:55:54 -0300

    commit teste

---
+++ src/HelloWord.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloWord {
+    public static void main(String [] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Usuário, digite seu nome: ");
+        String usuario = entrada.next();
+
+        System.out.print("Hello word " + usuario);
+    }
+}
